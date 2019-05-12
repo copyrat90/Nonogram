55501b8895699dfc179ba9b56268cf8cc8ceaed2
jun990115@naver.com <jun990115@naver.com>
2019-05-12 11:30:42 +0900

    Pages 폴더 생성. Pages 폴더 안에 StartupPage 페이지 생성 Pages 폴더 안에 PuzzlePlay 페이지 생성 PuzzlePlay 창 삭제

--- Nonogram/Pages/StartupPage.xaml.cs
+++ Nonogram/Pages/StartupPage.xaml.cs
@@ -10,16 +10,17 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Nonogram
+namespace Nonogram.Pages
 {
     /// <summary>
-    /// PuzzlePlay.xaml에 대한 상호 작용 논리
+    /// StartupPage.xaml에 대한 상호 작용 논리
     /// </summary>
-    public partial class PuzzlePlay : Window
+    public partial class StartupPage : Page
     {
-        public PuzzlePlay()
+        public StartupPage()
         {
             InitializeComponent();
         }
